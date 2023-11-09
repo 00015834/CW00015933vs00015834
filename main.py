75401b594072cea6f236172f9e0fc97972f7079e
Ezoza <ezozayuldasheva@Ezozas-MacBook-Air.local>
2023-11-09 16:52:30 +0500

    changes in main.py

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import matplotlib.pyplot as pltpip
 import os
 
 df = pd.read_csv(r"data.csv")
-print(df)
-df.describe
-print(df)
+print(df) #reading data
+
+
+df.columns #names of columns
+print(df.columns)
+
+df['GRADE'].max() #finding max value in GRADE column
+print(df['GRADE'].max())
+
+df.max() #print max values in data
+print(df.max())
 
 
 
