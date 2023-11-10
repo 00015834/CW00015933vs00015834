61329ffc09799b0af3519058d98bf8f1082c9a57
Ezoza <ezozayuldasheva@Ezozas-MacBook-Air.local>
2023-11-10 22:27:15 +0500

    changes in main.py

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
+#Reading dataset
 import pandas as pd
 import matplotlib.pyplot as pltpip
 import os
 
 df = pd.read_csv(r"data.csv")
-print(df) #reading data
+print(df)
+
+#Data exploration
+#1) number of rows and columns
+num_rows, num_columns = df.shape
+print(f"The dataset has {num_rows} rows and {num_columns} columns.")
+
+#2)available features (column names)
+features = df.columns.tolist()
+print("Available features:")
+print(features)
+
+#3)display summary information
+print(df.info())
+
+#Data Cleaning
+#drop a column
 
 
-df.columns #names of columns
-print(df.columns)
 
-df['GRADE'].max() #finding max value in GRADE column
-print(df['GRADE'].max())
 
-df.max() #print max values in data
-print(df.max())
+#Grouping data
+g = df.groupby('GRADE')
+print(g)
+
+for GRADE, GRADE_df in g:
+    print(GRADE)
+    print(GRADE_df)
+
+
+#d)
+
+
+
 
 
 
