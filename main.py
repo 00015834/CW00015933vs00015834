e83c1c1f175f4a11e3df2bf15ac19de7131105eb
Ezoza <ezozayuldasheva@Ezozas-MacBook-Air.local>
2023-11-11 15:08:58 +0500

    changes in main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 df = pd.read_csv(r"data.csv")
 print(df)
 
-# #Data exploration
-# #1) number of rows and columns
+ #Data exploration
+ #1) number of rows and columns
 num_rows, num_columns = df.shape
 print(f"The dataset has {num_rows} rows and {num_columns} columns.")
 
@@ -17,7 +17,7 @@ print("Available features:")
 print(features)
 
 #3)display summary information
-# print(df.info())
+print(df.info())
 
 #DATA Cleaning
 #drop a column(Reading frequency.1)
@@ -37,13 +37,7 @@ print(df)
 df = df.loc[df.duplicated()]
 print(df)
 
-#Grouping data
-# g = df.groupby('GRADE')
-# print(g)
-#
-# for GRADE, GRADE_df in g:
-#     print(GRADE)
-#     print(GRADE_df)
+
 
 
 #calculating average columns
